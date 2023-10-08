9542c83f876c002ea9c162a8911fd05a6340cd95
kimbeomyong <qkfmf@naver.com>
2023-10-09 00:26:59 +0900

    [edit deliveryProject]

--- deliveryProject/src/main/DeliveryMain.java
+++ deliveryProject/src/main/DeliveryMain.java
@@ -26,9 +26,9 @@ public class DeliveryMain {
 			System.out.println("----------------------------------------------");
 			System.out.println("\t welcome to delivery program");
 			System.out.println("----------------------------------------------");
-			System.out.println("1. 고객 정보 확인하기  \t 2. 장바구니 상품 목록 보기");
+			System.out.println("1. 고객 정보 확인하기  \t 2. 배달 목록 보기");
 			System.out.println("3. 장바구니 비우기    \t 4. 장바구니에 항목 추가하기");
-			System.out.println("5. 장바구니의 항목수량 줄이기  \t 6. 장바구닝의 항목 삭제하기");
+			System.out.println("5. 장바구니의 항목수량 줄이기  \t 6. 장바구니의 항목 삭제하기");
 			System.out.println("7. 영수증 표시하기    \t 8. 종료");
 
 			System.out.println("번호를 선택해주세요>>");
@@ -43,7 +43,7 @@ public class DeliveryMain {
 					System.out.println("현재 고객 정보");
 					System.out.println("이름 : " + name + ", 연락처 : " + phonenumber);
 				case 2:
-					System.out.println("2. 장바구니 상품 목록 보기 :");
+					System.out.println("2. 배달 목록 보기 :");
 					break;
 				case 3:
 					System.out.println("3. 장바구니 비우기 :");
