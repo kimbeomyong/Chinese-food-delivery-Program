9d6c8b23f98b3102ae94cf5ef012f253be41a963
kimbeomyong <qkfmf@naver.com>
2023-10-09 14:10:12 +0900

    [edit deliveryProject  addMenu]
    메뉴 추가

--- deliveryProject/src/main/DeliveryMain.java
+++ deliveryProject/src/main/DeliveryMain.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 
 public class DeliveryMain {
 
+	static final int NUM_MENU = 3;// 메뉴 갯수
+	static final int NUM_ITEM = 5;// 매뉴 정보 갯수
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);// 입력받기
 
 		String name;// 이름
 		int phoneNumber;// 전화번호
+		String adress; // 주소
 		int number;// 메뉴 번호 선택
+		String[][] menu = new String[NUM_MENU][NUM_ITEM];
 
 		System.out.println("고객 정보 입력");
 		System.out.println("이름 : ");
@@ -17,6 +22,9 @@ public class DeliveryMain {
 
 		System.out.println("전화번호 : ");
 		phoneNumber = scan.nextInt();// 전화번호를 정수로 입력받음
+
+		System.out.println("주소 : ");
+		adress = scan.next();
 
 		boolean quit = false;// 무한 반복
 
@@ -33,16 +41,16 @@ public class DeliveryMain {
 			} else {
 				switch (number) {
 				case 1:
-					menuInformation(name, phoneNumber);
+					menuInformation(name, phoneNumber, adress);
 					break;
 				case 2:
-					menuList();
+					menuItemList();
 					break;
 				case 3:
 					menuClear();
 					break;
 				case 4:
-					menuAdd();
+					menuAdd(menu);
 					break;
 				case 5:
 					menuCutDown();
@@ -64,7 +72,7 @@ public class DeliveryMain {
 
 	public static void menuIntroduction() {
 		System.out.println("----------------------------------------------");
-		System.out.println("\t welcome to delivery program");
+		System.out.println("\t  환영합니다. JAVA짜장면 입니다~");
 		System.out.println("----------------------------------------------");
 		System.out.println("1. 고객 정보 확인하기  \t 2. 배달 목록 보기");
 		System.out.println("3. 장바구니 비우기    \t 4. 장바구니에 항목 추가하기");
@@ -73,12 +81,12 @@ public class DeliveryMain {
 		System.out.println("----------------------------------------------");
 	}
 
-	public static void menuInformation(String name, int phoneNumber) {
+	public static void menuInformation(String name, int phoneNumber, String adress) {
 		System.out.println("현재 고객 정보");
-		System.out.println("이름 : " + name + ", 연락처 : " + phoneNumber);
+		System.out.println("이름 : " + name + ", 연락처 : " + phoneNumber + ",주소 : " + adress);
 	}
 
-	public static void menuList() {
+	public static void menuItemList() {
 		System.out.println("2. 배달 목록 보기 :");
 	}
 
@@ -86,8 +94,40 @@ public class DeliveryMain {
 		System.out.println("3. 장바구니 비우기 :");
 	}
 
-	public static void menuAdd() {
-		System.out.println("4. 장바구니에 항목 추가하기 :");
+	public static void menuAdd(String[][] menu) {
+		menuList(menu); // 메뉴 정보가 저장되어 있는 메서드 호출
+		// 메뉴 정보 출력
+		for (int i = 0; i < NUM_MENU; i++) {
+			for (int j = 0; j < NUM_ITEM; j++)
+				System.out.print(menu[i][j] + " | ");
+			System.out.println("");
+		}
+		boolean quit = false;
+		while (!quit) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("장바구니에 추가할 메뉴를 입력하세요 :");
+			String inputStr = input.nextLine();
+			boolean flag = false; // 일치 여부
+			int num = -1; // 인덱스 번호
+			for (int i = 0; i < NUM_MENU; i++) {
+				if (inputStr.equals(menu[i][0])) {
+					num = i;
+					flag = true;
+					break;
+				}
+			}
+			// 일치하면 장바구니 추가 여부를 묻는다.
+			if (flag) {
+				System.out.println("장바구니에 추가하겠습니까? Y | N ");
+				inputStr = input.nextLine();
+				if (inputStr.toUpperCase().equals("Y") || inputStr.toUpperCase().equals("y")) {
+					System.out.println(menu[num][0] + " 메뉴가 장바구니에추가되었습니다.");
+				}
+				quit = true;
+
+			} else
+				System.out.println("다시 입력해 주세요");
+		}
 	}
 
 	public static void menuCutDown() {
@@ -106,4 +146,23 @@ public class DeliveryMain {
 		System.out.println("8. 종료");
 	}
 
+	public static void menuList(String[][] menu) {
+		menu[0][0] = "짜장면";
+		menu[0][1] = "별점 : 4.9점";
+		menu[0][2] = "호로록 끊임없이 넘어가는 단맛이 덜한 옛날짜장";
+		menu[0][3] = "7,000 원";
+		menu[0][4] = "배달시간 : 31~ 46분 소요 예상";
+
+		menu[1][0] = "짬뽕";
+		menu[1][1] = "별점 : 4.2점";
+		menu[1][2] = "강한불에 해물과 야채가 만나 시원함 UP!!UP!!";
+		menu[1][3] = "8,000 원";
+		menu[1][4] = "배달시간 : 31 ~ 46분 소요 예상";
+
+		menu[2][0] = "탕수육";
+		menu[2][1] = "별점 : 4.8점";
+		menu[2][2] = "★ 사장님이 강력 추천하는 메뉴!! ★";
+		menu[2][3] = "22,000 원";
+		menu[2][4] = "배달시간 : 31 ~ 46분 소요 예상";
+	}
 }
